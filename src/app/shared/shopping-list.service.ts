2ab3d62d7966c6486c71a7b5bb39f65db7342865
Pratima Sakinala <pratimasakinala@Pratimas-MacBook-Pro.local>
2018-06-25 11:05:28 -0700

    using push notifications to update shopping list

--- src/app/shared/shopping-list.service.ts
+++ src/app/shared/shopping-list.service.ts
@@ -1,3 +1,5 @@
+import { EventEmitter } from "@angular/core";
+
 import { Ingredient } from "./ingredient.model";
 
 export class ShoppingListService {
@@ -9,11 +11,14 @@ export class ShoppingListService {
         new Ingredient('Tomatoes', 10)
     ];
 
+    ingredientChanged = new EventEmitter<Ingredient[]>();
+
     getIngredients(): Ingredient[] {
         return this.ingredients.slice();
     }
 
     addIngredient(ingredient: Ingredient) {
         this.ingredients.push(ingredient);
+        this.ingredientChanged.emit(this.ingredients.slice());
     }
 }
